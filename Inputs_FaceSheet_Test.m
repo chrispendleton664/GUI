edd514006c6ca9c537d871bc0eaa62622ec097e6
Gerico Vidanes <gericov@outlook.com>
2021-04-11 20:15:25 +0100

    Test for accommodating existing input files
    So users don't have to type values each time

---
+++ Inputs_FaceSheet_Test.m
@@ -0,0 +1,27 @@
+function Inputs = Inputs_FaceSheet_Test()
+
+Inputs.No_Plies = 7;
+Inputs.Theta = [0 90 0 90 0 90 0];
+
+Inputs.E1 = 24.14E9;
+Inputs.E2 = 24.14E9;
+Inputs.G12 = 3.79E9;
+Inputs.v12 = 0.11;
+Inputs.a = 0.22;
+Inputs.b = 0.22;
+Inputs.h = 1.96e-3;
+Inputs.rho = 1800; 
+
+Inputs.Xt = 2207e6;
+Inputs.Xc = -1531e6;
+Inputs.Yt = 80.7e6;
+Inputs.Yc = -199.8e6;
+Inputs.S = 114.5e6;
+
+Inputs.ext = 4e-3;
+Inputs.exc = -3e-3;
+Inputs.eyt = 4e-3;
+Inputs.eyc = -3e-3;
+Inputs.es = 6e-3;
+
+end
